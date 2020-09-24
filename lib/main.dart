f7e3aeeea580cc32fc28740f5624229959905879
Demartechx <adeagbothompson@gmail.com>
2020-09-24 13:38:09 +0100

    Buttons and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,70 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
-
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Center(
-        child: Text('my first app')
+      appBar: AppBar(
+        title: Center(child: Text('my first app')),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      /*
-      child: Image(
-        image: AssetImage('images/img1.jpg'),
+      body: Center(
+        //clickable icon buttons
+        child: IconButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
         ),
 
-        child: Image.network('images/img1.jpg'),
+        /*
+        //button with icon
+        child: RaisedButton.icon( 
+          onPressed: () {
+            print('you clicked me');
+          },
+          icon: Icon(
+            Icons.mail,
+            color: Colors.blue[900],
+          ),
+          label: Text(
+            'Mail me',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue[800]
+            ),
+          ),
+          color: Colors.amber,
+        ),
+      
+        //button
+        child: FlatButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          child: Text('click me'),
+          color: Colors.lightBlue,
+        ),
+       
+      //Icon
+      child: Icon(
+        Icons.airport_shuttle,
+        color: Colors.lightBlue,
+        size: 50.0
+      ),
       */
-
-      child: Image.asset('images/img1.jpg'),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
