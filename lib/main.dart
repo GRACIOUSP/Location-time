15605f5cc2030e441a930a60c2c1b65bc78f1ffe
Demartechx <adeagbothompson@gmail.com>
2020-09-25 14:36:50 +0100

    Ninja ID card

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: NinjaCard(),
     ));
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Center(child: Text('my first app')),
+        title: Text('Ninja ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-
-      body: Row(
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                child: Image.asset('images/img2.jpg'),
-                flex: 3,
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Colors.cyan,
-                  child: Text('1')
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('images/matt.jpg'),
+                  radius: 50.0,
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Colors.pinkAccent,
-                  child: Text('2')
+               Divider(
+                 height: 90.0,
+                 color: Colors.grey[600],
+               ),
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Colors.amber,
-                  child: Text('3')
+              SizedBox( height: 20.0), // give space
+              Text(
+                'Matthew',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold
                 ),
+              ),
+              SizedBox( height: 30.0), // give space
+
+              Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+                ),
+              ),
+              SizedBox( height: 20.0), // give space
+              Text(
+                '8',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox( height: 30.0), // give vertical space
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400]
+                  ),
+                  SizedBox( width: 10.0), // give horizontal space
+                  Text(
+                    'adeagbothompson@gmail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      letterSpacing: 1.0,
+                      fontSize: 10.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
               ),
             ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.red[600],
-      ),
+          ),
+        ),
     );
   }
 }
